3f20afa5b729b35c38f0213c8fdd6297e51cff1c
Cassie003 <caseyminu@gmail.com>
2022-03-05 12:42:25 +0300

    Collecting data from input

---
+++ script.js
@@ -0,0 +1,8 @@
+const arrayAkanMale = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'];
+const arrayAkanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
+// Collecting data from input
+    function getDate() {
+        let dateInput = document.getElementById('dob').value;
+        console.log(dateInput);
+        male = document.getElementById('maleRadio'); // collecting from male radio
+        female = document.getElementById('femaleRadio');
