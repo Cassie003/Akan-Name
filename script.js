1784460fc29f2b889331f91c8c7f2e4e35db6600
Cassie003 <caseyminu@gmail.com>
2022-03-05 12:43:04 +0300

    Parsing the date

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const arrayAkanFemale = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Am
         console.log(dateInput);
         male = document.getElementById('maleRadio'); // collecting from male radio
         female = document.getElementById('femaleRadio');
+        if(dateInput == '') {
+            alert('Please enter a valid date');
+        } else if (male.checked == false && female.checked == false) {
+            alert('Please select gender');
+            
+        } else {
+    
+                // Parsing the date
+                date = new Date(dateInput);
+                console.log(date);
+    
